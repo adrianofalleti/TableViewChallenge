33f7b499fb842fb8f86893d88d00d43ee89f72fe
Adriano Falleti <adrianofalleti@MacBook-Pro-di-Adriano.local>
2015-08-28 12:22:15 +0200

    Initial Commit

---
+++ TableVIewChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableVIewChallenge
+//
+//  Created by Adriano Falleti on 28/08/15.
+//  Copyright (c) 2015 Adriano Falleti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
